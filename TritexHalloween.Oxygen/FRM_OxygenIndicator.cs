fec3d9baae9e7e5bbd1794d581c4e0f340646879
matt.shakespeare@allburg.com <matt.shakespeare@allburg.com>
2018-10-27 18:36:04 +0100

    Updated Heart Rate, and Oxygen

---
+++ TritexHalloween.Oxygen/FRM_OxygenIndicator.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TritexHalloween.Oxygen
+{
+    using System.IO;
+    using System.Net;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Threading;
+
+    using TritexHalloween.Networking;
+    using TritexHalloween.StoryObjects;
+
+    public partial class FRM_OxygenIndicator : Form
+    {
+        private IPAddress ip;
+
+        private Adventurer adventurer;
+
+        private Random heartRateRandom;
+
+        public FRM_OxygenIndicator(string ip)
+        {
+            this.ip = IPAddress.Parse(ip);
+           
+            InitializeComponent();
+            this.Server.RunWorkerAsync();
+            this.heartRateRandom = new Random();
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+        }
+
+        private void FRM_OxygenIndicator_Load(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Normal;
+            //this.Location = new Point(0, 0);
+            //this.TopMost = true;
+            //Screen currentScreen = Screen.FromHandle(this.Handle);
+            //this.Size = new System.Drawing.Size(currentScreen.Bounds.Width, currentScreen.Bounds.Height);
+        }
+
+        private void Server_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var server = new Server(this.ip, this.UpdateAdventurer);
+            server.Start();
+        }
+
+        private void UpdateAdventurer(Message message)
+        {
+            if (message.ObjectType == 0)
+            {
+                IFormatter formatter;
+                using (var stream = new MemoryStream(message.Data))
+                {
+                    formatter = new BinaryFormatter();
+                    var adventurer = (Adventurer)formatter.Deserialize(stream);
+                    this.Invoke((MethodInvoker)delegate
+                        {
+                            this.SetAdventurer(adventurer);
+                        });
+                }
+            }
+        }
+
+        private void SetAdventurer(Adventurer adventurer)
+        {
+            this.tmr_respiration.Stop();
+
+            this.adventurer = adventurer;
+            this.lb_SubjectValue.Text = adventurer.Name;
+            this.lb_temperature.Text = adventurer.Temperature.ToString("N2") + "C";
+            this.lb_suitpressure.Text = adventurer.SuitPressure + "-Bar";
+            this.lb_oxygen.Text = adventurer.OxygenRemaining.ToString("N2") + "L";
+            this.tmr_respiration.Start();
+        }
+
+        private void setOxygenHeartRate()
+        {
+            var currentHeartRate = 0;
+
+            switch (this.adventurer.HeartRateMode)
+            {
+                default:
+                case HeartRateMode.Error:
+                    break;
+                case HeartRateMode.BelowResting:
+                    currentHeartRate = this.heartRateRandom.Next(50, 60);
+                    break;
+                case HeartRateMode.Resting: // 60
+                    currentHeartRate = this.heartRateRandom.Next(60, 65);
+                    break;
+                case HeartRateMode.AboveResting:
+                    currentHeartRate = this.heartRateRandom.Next(72, 85);
+                    break;
+                case HeartRateMode.Stressed:
+                    currentHeartRate = this.heartRateRandom.Next(72, 100);
+                    break;
+                case HeartRateMode.ModerateExercise: // 
+                    currentHeartRate = this.heartRateRandom.Next(100, 130);
+                    break;
+                case HeartRateMode.High: // 16
+                    currentHeartRate = this.heartRateRandom.Next(140, 160);
+                    break;
+                case HeartRateMode.Extreme: // 20
+                    currentHeartRate = this.heartRateRandom.Next(160, 200);
+                    break;
+            }
+
+            this.adventurer.OxygenRemaining -= (currentHeartRate / 10m * (0.8m + (currentHeartRate / 1000m))) / 60m;
+            this.lb_oxygen.Text = this.adventurer.OxygenRemaining.ToString("n2");
+            this.lb_heartrate.Text = currentHeartRate.ToString();
+        }
+
+        private void tmr_respiration_Tick(object sender, EventArgs e)
+        {
+            this.setOxygenHeartRate();
+        }
+    }
+}
---
+++ TritexHalloween.Oxygen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace TritexHalloween.Oxygen
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string ip = "127.0.0.1";
+
+            var form = new FRM_OxygenIndicator(ip);
+            Application.Run(form);
+        }
+    }
+}
